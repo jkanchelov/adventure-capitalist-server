78d77c8cd8e176d4e9eba6b842c450f93f846f92
jkanchelov <jkanchelov@gmail.com>
2020-07-12 16:26:44 +0300

    Update lemenBusiness settings

--- src/game/business/LemonBusiness.ts
+++ src/game/business/LemonBusiness.ts
@@ -1,11 +1,15 @@
 import Business from "./Business";
 
+const earningPerShop = 5;
+const timePerCycle = 1000;
+const initialShopPrice = 10;
+
 export default class LemonBusiness extends Business {
     /**
      *
      */
     constructor() {
-        super(100, 1000);
+        super(earningPerShop, timePerCycle, initialShopPrice);
 
         console.log("Lemon Business initialized");
     }
