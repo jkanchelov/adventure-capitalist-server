b8705b5cea456f44a61d13ad6a2d2d5753209120
jkanchelov <jkanchelov@gmail.com>
2020-07-12 16:23:15 +0300

    Implement missing methods from the business class

--- src/game/business/Business.ts
+++ src/game/business/Business.ts
@@ -3,21 +3,30 @@ import Manager from "../interfaces/manager";
 
 export default abstract class Business implements IBusiness {
     name: string;
-    shopsQuantity: number;
     manager: Manager;
-    completeJobTime: number;
     money: number;
+    shopsQuantity: number;
 
-    protected _isWorking = false;
-    protected _profitPerCycle: number;
+    protected _isWorking: boolean;
+    protected _earningPerShop: number;
     protected _timePerCycle: number;
-    /**
-     *
-     */
-    protected constructor(profitPerCycle: number, timePerCycle: number) {
-        this._profitPerCycle = profitPerCycle;
-        this._timePerCycle = timePerCycle;
+    protected _startedJobTime: Date;
+    protected _initialShopPrice: number;
+    protected _upgrades: number[]; // upgrade values to multiply the profit with;
+
+    protected constructor(
+        earningPerShop: number,
+        timePerCycle: number,
+        shopPrice: number
+    ) {
         this.money = 0;
+        this.shopsQuantity = 1;
+
+        this._earningPerShop = earningPerShop;
+        this._timePerCycle = timePerCycle;
+        this._isWorking = false;
+        this._initialShopPrice = shopPrice;
+        this._upgrades = [];
     }
 
     addManager(manager: Manager): void {
@@ -29,27 +38,59 @@ export default abstract class Business implements IBusiness {
 
     start(): void {
         this._isWorking = true;
+        this._startedJobTime = new Date();
 
         setTimeout(() => {
-            this._isWorking = false;
-
-            this.money += this._profitPerCycle;
+            this.finishJob();
         }, this._timePerCycle);
+    }
+
+    remainingCompleteJobTime(): number {
+        if (!this._startedJobTime) {
+            // business not working
+            return this._timePerCycle;
+        } else {
+            const timePassed =
+                new Date().getTime() - this._startedJobTime.getTime();
+
+            return this._timePerCycle - timePassed;
+        }
     }
 
     isWorking(): boolean {
         return this._isWorking;
     }
 
-    upgrade(): void {
-        throw new Error("Method not implemented.");
+    buyShop(): void {
+        this.shopsQuantity += 1;
     }
 
-    setProfitPerCycle(profitPerCycle: number): void {
-        this._profitPerCycle = profitPerCycle;
+    upgrade(upgradeMultiplier: number): void {
+        this._upgrades.push(upgradeMultiplier);
+    }
+
+    nextShopPrice(): number {
+        return this.shopsQuantity * this._initialShopPrice;
     }
 
     setTimePerCycle(timePerCycle: number): void {
         this._timePerCycle = timePerCycle;
+    }
+
+    protected finishJob() {
+        this._isWorking = false;
+        this._startedJobTime = null;
+
+        this.money += this.calculateEarnedMoney();
+    }
+
+    protected calculateEarnedMoney(): number {
+        let earnedMoney = this._earningPerShop * this.shopsQuantity;
+
+        this._upgrades.forEach(upgradeMultiplier => {
+            earnedMoney *= upgradeMultiplier;
+        });
+
+        return earnedMoney;
     }
 }
